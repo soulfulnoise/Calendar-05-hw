bb35426e63323dced0da5eed7c0b0035d4a769a3
The Noise <leespaulding25@gmail.com>
2021-01-23 13:36:45 -0500

    updated Html rows and added Jquery

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,32 @@
+//box coloring
 
-    
+console.log(moment().format('LLL'));
+
+$(".form-control").each(function () {
+    console.log($(this).attr("time"));
+
+    var time =$(this).attr("time");
+
+    if(time < moment().hour()){ 
+        $(this).addClass("past");
+   
+        console.log("past")
+        
+}
+
+    if(time == moment().hour()){
+        $(this).addClass("present");
+        console.log("present")
+    }
+
+    if(time > moment().hour()){
+        $(this).addClass("future");
+        console.log("future")
+    }
+
+    //Saving to localStorage
+   
+  
+
+});
+
